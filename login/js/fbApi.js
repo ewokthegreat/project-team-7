72d90ba2efa1a11d6aba8f149ee9a971f39530ee
ewokthegreat <eric@ewokthegreat.com>
2016-04-05 21:46:16 -0400

    fbApi.js demonstrates how to make the ajax request.

--- login/js/fbApi.js
+++ login/js/fbApi.js
@@ -1,6 +1,27 @@
 /**
  * Created by ewokthegreat on 3/21/2016.
  */
+
+//iife to test some things out
+(function(window, document) {
+    'use strict';
+
+    console.log("Creating request object...");
+
+    var xhr = new XMLHttpRequest();
+    console.log(xhr);
+    xhr.open('POST', '/spider/login/php/test.php');
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.onload = function() {
+        console.log(xhr.responseText);
+    };
+    xhr.send(JSON.stringify({
+        message: 'TESTING',
+        type: 'SEND'
+    }));
+
+})(window, document);
+
 window.fbAsyncInit = function() {
     FB.init({
         appId      : '1679655878969496', //Social Spider Test appID
---
+++ login/php/test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ewokthegreat
+ * Date: 4/5/2016
+ * Time: 9:26 PM
+ */
+echo 'dick';
+?>
