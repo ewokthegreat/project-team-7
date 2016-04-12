97f2fa97c3aa0611aa6c4b46c31f6f99a927b45b
ewokthegreat <eric@ewokthegreat.com>
2016-04-11 22:25:33 -0400

    Added a helper file for Adam

---
+++ e_adam_testing/php/adamhelper.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ewokthegreat
+ * Date: 4/11/2016
+ * Time: 10:17 PM
+ */
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+$pathToData = "/home/ewoktheg/public_html/spider/eric_test_playground/.raw_user_data/10208453844209830/16.04.10.23.52.59__10208453844209830.json";
+
+$data = file_get_contents($pathToData);
+
+
+?>
+<!doctype HTML>
+<html>
+<head>
+
+</head>
+<body>
+
+</body>
+</html>
