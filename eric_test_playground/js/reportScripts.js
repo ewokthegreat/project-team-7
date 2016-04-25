d86b30d561cf137e2c94a2679617db7caf7dd3a4
ewokthegreat <eric@ewokthegreat.com>
2016-04-25 16:44:43 -0400

    Got more stuff working.

--- eric_test_playground/js/reportScripts.js
+++ eric_test_playground/js/reportScripts.js
@@ -2,8 +2,27 @@
  * Created by ewokthegreat on 4/25/2016.
  */
 (function(window, document, undefined) {
-    var report = window.localStorage.getItem('report');
+    //Get the ace data from localStorage
+    var reportData = window.localStorage.getItem('report');
+    //Remove the ace data from localStorage
+    window.localStorage.removeItem('report');
+
+    var report = JSON.parse(reportData);
+
+    var freqArr = report.intervalFlaggedPosts;
     
-    console.log(JSON.parse(report));
+    var jsonFreq = [];
+    for(var date in freqArr) {
+        var obj = {};
+
+        obj.date = date;
+        obj.count = freqArr[date];
+
+        jsonFreq.push(obj);
+    }
+    
+    
+    var bubbleChart = new BubbleChart(report.bubbleGraphData, '#bubble-chart');
+    var lineChart = new LineChart(jsonFreq, '#freq-graph', '#freq-slider-range');
     
 }(window, document, undefined));
