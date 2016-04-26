9906b300a918125f751777952b9783d1ffbac544
ewokthegreat <eric@ewokthegreat.com>
2016-04-26 04:44:45 -0400

    More styling and dynamic content loading.

--- eric_test_playground/js/reportScripts.js
+++ eric_test_playground/js/reportScripts.js
@@ -15,6 +15,7 @@
     var freqArr = report.intervalFlaggedPosts;
 
     console.log(applicant);
+    console.log(report);
 
     var jsonFreq = [];
     for(var date in freqArr) {
@@ -34,11 +35,37 @@
     var lname = document.getElementById('last-name');
     var email = document.getElementById('profile-email');
     var date = document.getElementById('profile-scan-date');
-    var profileLink = document.getElementById('profile-link');
+    var profileLinks = document.getElementById('profile-links');
+    var fbLink = document.querySelector('#profile-links .fb-link');
+    var freqMetric = document.getElementById('frequency-metric');
+    var qualityMetric = document.getElementById('quality-metric');
+    var scanDate = document.querySelector('.dl-horizontal .scan-date');
+    var firstFlag = document.querySelector('.dl-horizontal .first-flag');
+    var lastFlag = document.querySelector('.dl-horizontal .last-flag');
+    var favoriteTeam = document.querySelector('.dl-horizontal .fav-team');
 
+    fbLink.href = applicant.profileLink;
     picture.style.backgroundImage = 'url(' + applicant.profilePicture + ')';
     fname.innerHTML = applicant.firstName;
     lname.innerHTML = applicant.lastName;
+    email.innerHTML = applicant.email;
+    freqMetric.innerHTML = report.flaggedPostsPerYear.toFixed(2);
+    qualityMetric.innerHTML = report.averageWeightOfFlaggedPost.toFixed(2);
+
+    var dateArray = report.dateGenerated.split('.');
+    var date = new Date(dateArray[1] + ' ' + dateArray[2] + ' ' + 20 + dateArray[0]);
+    scanDate.innerHTML = date;
+
+    dateArray = report.firstFlaggedPostDate.split('-');
+    date = new Date(dateArray[2] + ' ' + dateArray[1] + ' ' + dateArray[0]);
+    firstFlag.innerHTML = date;
+
+    dateArray = report.lastFlaggedPostDate.split('-');
+    date = new Date(dateArray[2] + ' ' + dateArray[1] + ' ' + dateArray[0]);
+    lastFlag.innerHTML = date;
+
+    favoriteTeam.innerHTML = report.favoriteTeam.charAt(0).toUpperCase() + report.favoriteTeam.slice(1);
+
 
     function _getCurrentApplicantData(id, data) {
         for(var i = 0; i < data.length; i++) {
@@ -49,5 +76,7 @@
             }
         }
     }
+
+
 
 }(window, document, undefined));
